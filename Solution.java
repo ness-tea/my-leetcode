7dba84b3bbe307f6c0ed62ad927d8db3a7cf5a8e
Vanessa Truong <truonv1@mcmaster.ca>
2018-01-06 20:29:11 -0500

    deleted file

--- Solution.java
+++ Solution.java
@@ -18,15 +18,4 @@ class Solution {
         
         return arrlength;
     }
-    public static void main(String[] args) {
-    	int[] nums = new int[5];
-    	nums[0] = 0;
-    	nums[1] = 2;
-    	nums[2] = 3;
-    	nums[3] = 4;
-    	nums[4] = 5;
-    	
-    	System.out.println(nums.length);
-    	System.out.println(removeDuplicates(nums));
-    }
 }
