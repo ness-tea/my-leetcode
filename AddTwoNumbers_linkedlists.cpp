0d5232880205017b9c760d4e54bb917a0b11c8fd
nessa-art <nessenceart@gmail.com>
2019-10-06 01:15:51 -0400

    commenting AddTwoNumbers.cpp

--- AddTwoNumbers_linkedlists.cpp
+++ AddTwoNumbers_linkedlists.cpp
@@ -1,3 +1,9 @@
+/** Vanessa Truong **/
+/** Add Two Numbers
+
+    Given two numbers represented as linked lists, add them and store the result in a third linked list. 
+**/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,12 +15,17 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+	// Track sum of each digt place.  Also used to calculate carry-over amount.
         int sum = 0;
+	
+	// Create new node to store digit sum (beginning ones place) and store in a temp pointer to operate on.
         ListNode* result = new ListNode(0);
         ListNode* temp = result;
         
+	// Check that lists are not empty before operating on.
         while (l1 != NULL || l2 != NULL)
         {
+            // Add value of each list to sum per digit place if value exists.
             if (l1 != NULL)
             {
                 sum += l1->val;
@@ -27,10 +38,12 @@ public:
                 l2 = l2->next;
             }
             
+	    // Store value of sum modulo 10 in temp node. If sum > 10, reassign sum to sum/10 to carry-over to next digit place sum. 
             temp->val = sum%10;
             sum = sum/10;
             
-            if (l1 != NULL || l2 != NULL || sum > 0)
+	    // If there is carry-over left, create new node to store carry-over amount in.
+            if (sum > 0)
             {
                 temp->next = new ListNode(sum);
                 temp = temp->next;
